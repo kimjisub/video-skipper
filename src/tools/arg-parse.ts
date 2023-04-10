73180629552153b5c642148a9b20629d650a6e13
kimjisub <0226daniel@gmail.com>
2023-04-10 15:21:49 +0900

    feat: add dest

--- src/tools/arg-parse.ts
+++ src/tools/arg-parse.ts
@@ -6,6 +6,7 @@ const parser = new ArgumentParser({
 
 parser.add_argument('-i', '--input', {
 	type: String,
+	dest: 'input',
 	metavar: 'INPUT',
 	help: 'Input file. (Default: input.mp4)',
 	default: 'input.mp4',
@@ -13,6 +14,7 @@ parser.add_argument('-i', '--input', {
 
 parser.add_argument('-o', '--output', {
 	type: String,
+	dest: 'output',
 	metavar: 'OUTPUT',
 	help: 'Output file. (Default: output.mp4)',
 	default: 'output.mp4',
@@ -20,6 +22,7 @@ parser.add_argument('-o', '--output', {
 
 parser.add_argument('-s', '--sounded_speed', {
 	type: Number,
+	dest: 'sounded_speed',
 	metavar: 'SOUNDED_SPEED',
 	help: 'Set sounded part speed. (Default: 1)',
 	default: 1,
@@ -27,6 +30,7 @@ parser.add_argument('-s', '--sounded_speed', {
 
 parser.add_argument('-ns', '--silent_speed', {
 	type: Number,
+	dest: 'silent_speed',
 	metavar: 'SILENT_SPEED',
 	help: 'Set silent part speed. (Default: Inf)',
 	default: Infinity,
@@ -34,6 +38,7 @@ parser.add_argument('-ns', '--silent_speed', {
 
 parser.add_argument('-db', '--standard_db', {
 	type: Number,
+	dest: 'standard_db',
 	metavar: 'STANDARD_dB',
 	help: 'Set standard dB for recognition. (Default: -50)',
 	default: -50,
@@ -41,6 +46,7 @@ parser.add_argument('-db', '--standard_db', {
 
 parser.add_argument('-vrr', '--volume-round-range', {
 	type: Number,
+	dest: 'volume_round_range',
 	metavar: 'RANGE',
 	help: 'Set volume round range in chunk. (Default: 3)',
 	default: 3,
@@ -48,6 +54,7 @@ parser.add_argument('-vrr', '--volume-round-range', {
 
 parser.add_argument('-srr', '--sounded-round-range', {
 	type: Number,
+	dest: 'sounded_round_range',
 	metavar: 'RANGE',
 	help: 'Set sounded round range in chunk. (Default: 10)',
 	default: 10,
@@ -55,6 +62,7 @@ parser.add_argument('-srr', '--sounded-round-range', {
 
 parser.add_argument('-vrm', '--volume-round-method', {
 	type: Number,
+	dest: 'volume_round_method',
 	metavar: 'METHOD',
 	help: 'Set volume round method. (Default: 3)',
 	default: 3,
@@ -63,6 +71,7 @@ parser.add_argument('-vrm', '--volume-round-method', {
 
 parser.add_argument('-srm', '--sounded-round-method', {
 	type: Number,
+	dest: 'sounded_round_method',
 	metavar: 'METHOD',
 	help: 'Set sounded round method. (Default: 1)',
 	default: 1,
@@ -71,6 +80,7 @@ parser.add_argument('-srm', '--sounded-round-method', {
 
 parser.add_argument('-d', '--debug', {
 	type: Boolean,
+	dest: 'debug',
 	metavar: 'DEBUG',
 	help: 'Show Debug web page. (Default: true)',
 	default: true,
