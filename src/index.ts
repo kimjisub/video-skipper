88ad781dc765bdd84a1052cec4c4d357eba7d34d
kimjisub <0226daniel@gmail.com>
2023-05-01 22:42:34 +0900

    feat: arg parser

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { plot } from 'nodeplotlib';
 import getVolumes from './ffmpeg/volume';
 import ResultPlot from './tools/result-plot';
+import { parseArgs } from './tools/arg-parse';
+
+const args = parseArgs();
 
 start();
 
 async function start() {
 	console.log('1. Analyzing Volume of Video');
 
-	const volumes = await getVolumes('input.mp4');
+	const volumes = await getVolumes(args.input);
 
 	const plot = new ResultPlot();
 
